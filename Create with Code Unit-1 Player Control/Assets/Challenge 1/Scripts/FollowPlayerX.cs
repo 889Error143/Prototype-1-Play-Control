498bc160d5480aa067b3f765300fc7b86a97c4c6
889Error143 <113727616+889Error143@users.noreply.github.com>
2023-09-28 14:25:32 -0700

    Prototype 1

---
+++ Create with Code Unit-1 Player Control/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject ObjectToFollow;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - ObjectToFollow.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = ObjectToFollow.transform.position + offset;
+    }
+}
---
+++ Create with Code Unit-1 Player Control/Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Playercontroller : MonoBehaviour
+{
+
+    public float Speed = 5f;
+    public float RotationSpeed = 45f;
+
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _verticalInput = Input.GetAxis("Vertical");
+        _horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Speed * _verticalInput * Time.deltaTime);
+
+        // turn the vehicle
+
+        transform.Rotate(Vector3.up, RotationSpeed * _horizontalInput * Time.deltaTime);
+
+    }
+
+    
+ 
+     
+    
+}
